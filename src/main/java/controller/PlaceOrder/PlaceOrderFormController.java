3a0191645b816212592551ca2beb9a74f4e0b186
chathuraruwantha-2002 <143559027+chathuraruwantha-2002@users.noreply.github.com>
2025-02-04 06:16:21 +0530

    commit PlaceOrder CRUD
    product table load done

--- src/main/java/controller/PlaceOrder/PlaceOrderFormController.java
+++ src/main/java/controller/PlaceOrder/PlaceOrderFormController.java
@@ -1,23 +1,49 @@
 package controller.PlaceOrder;
 
+import com.jfoenix.controls.JFXButton;
 import controller.Cards.ProductCardController;
 import controller.Products.ProductsController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import com.jfoenix.controls.JFXTextField;
+import model.Inventory;
 import model.Product;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class PlaceOrderFormController implements Initializable {
+
+    @FXML
+    private TableView TableProductsView;
+
+    @FXML
+    private TableColumn colActions;
+
+    @FXML
+    private TableColumn colProductName;
+
+    @FXML
+    private TableColumn colQty;
+
+    @FXML
+    private TableColumn colTotal;
+
+
     @FXML
     private GridPane ProductsContainer;
 
@@ -36,6 +62,8 @@ public class PlaceOrderFormController implements Initializable {
     @FXML
     private JFXTextField searchCustomer;
 
+    private List<Product> SelectedProductList = new ArrayList<>();
+
 
 
     @Override
@@ -43,6 +71,11 @@ public class PlaceOrderFormController implements Initializable {
         LoadGridCards(new ProductsController().GetAllProducts());
         this.date.setText(java.time.LocalDate.now().toString());
         this.orderId.setText(String.format("O%03d", new PlaceOrderController().getTopOrderID()+1));
+
+        //set table column names
+        colProductName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colQty.setCellValueFactory(new PropertyValueFactory<>("qty"));
+        colTotal.setCellValueFactory(new PropertyValueFactory<>("price"));
 
     }
 
@@ -86,9 +119,22 @@ public class PlaceOrderFormController implements Initializable {
         customerName.setText(new PlaceOrderController().getCustomerName(searchCustomer.getText()));
     }
 
+    //rewrite this again
     public void DisplayOrderDetailsOnSide (Product product) {
+        if(!SelectedProductList.contains(product)){
+            SelectedProductList.add(product);
+        } else{
+            SelectedProductList.get(SelectedProductList.indexOf(product)).setQty(SelectedProductList.get(SelectedProductList.indexOf(product)).getQty()+1);
+        }
+        LoadProductsTable();
         System.out.println(product);
+    }
 
+    private void LoadProductsTable() {
+        TableProductsView.getItems().clear();
+        ObservableList<Product> itemsList = FXCollections.observableArrayList();
+        itemsList.addAll(SelectedProductList);
+        TableProductsView.setItems(itemsList);
     }
 
 }
