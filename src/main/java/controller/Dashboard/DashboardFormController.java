a3293f3b7f2c651a60dd4ecda6019cb458676766
chathuraruwantha-2002 <143559027+chathuraruwantha-2002@users.noreply.github.com>
2025-02-09 23:03:19 +0530

    Commit Dashboard
    CRUD

--- src/main/java/controller/Dashboard/DashboardFormController.java
+++ src/main/java/controller/Dashboard/DashboardFormController.java
@@ -1,11 +1,29 @@
 package controller.Dashboard;
 
 import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
-public class DashboardFormController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardFormController implements Initializable {
 
     @FXML
-    private ImageView Dashbordicon;
+    private Label activeLabel;
 
+    @FXML
+    private Label alertsLabel;
+
+    @FXML
+    private Label returnsLabel;
+
+    @FXML
+    private Label salesLabel;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
