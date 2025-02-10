877ac953eabd1586fecf40c6fb4bd1d9252dfd1b
chathuraruwantha-2002 <143559027+chathuraruwantha-2002@users.noreply.github.com>
2025-02-11 05:29:36 +0530

    commit Login
    validations done

--- src/main/java/controller/login/LoginController.java
+++ src/main/java/controller/login/LoginController.java
@@ -1,4 +1,34 @@
 package controller.login;
 
+import DBConnection.DBConnection;
+import model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class LoginController {
+
+    public User getUserDetails(String UserEmail){
+        String query = "SELECT * FROM user WHERE Email = ? ";
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1,UserEmail);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                User user = new User(
+                        resultSet.getInt(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4)
+                );
+                return user;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
 }
--- src/main/java/controller/login/LoginFormController.java
+++ src/main/java/controller/login/LoginFormController.java
@@ -1,4 +1,36 @@
 package controller.login;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
+import model.User;
+
 public class LoginFormController {
+
+    @FXML
+    private JFXTextField userEmail;
+
+    @FXML
+    private JFXPasswordField userPassword;
+
+
+    public void fLogin(MouseEvent mouseEvent) {
+        User user = new LoginController().getUserDetails(userEmail.getText());
+
+        if(userEmail.getText().isEmpty() && userPassword.getText().isEmpty()){
+            new Alert(Alert.AlertType.ERROR,"Please Enter Email and Password.!!").show();
+        }else if(userEmail.getText().isEmpty()){
+            new Alert(Alert.AlertType.ERROR,"Please Enter Email.!!").show();
+        } else if (userPassword.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Please Enter Password.!!").show();
+        }else if(user == null){
+            new Alert(Alert.AlertType.ERROR,"User Not Found.!!").show();
+        }else if(user.getPassword().equals(userPassword.getText())) {
+            new Alert(Alert.AlertType.INFORMATION,"Login Success.!!").show();
+        }else{
+            new Alert(Alert.AlertType.ERROR,"Invalid Password.!!").show();
+        }
+    }
 }
