44730aa3b878c3e7e526b915a645bac658e40e9a
chathuraruwantha-2002 <143559027+chathuraruwantha-2002@users.noreply.github.com>
2025-02-11 07:00:32 +0530

    Commit Login functions
    stages

--- src/main/java/controller/login/LoginFormController.java
+++ src/main/java/controller/login/LoginFormController.java
@@ -3,9 +3,14 @@ package controller.login;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import model.User;
+
+import java.io.IOException;
 
 public class LoginFormController {
 
@@ -16,7 +21,7 @@ public class LoginFormController {
     private JFXPasswordField userPassword;
 
 
-    public void fLogin(MouseEvent mouseEvent) {
+    public void fLogin(MouseEvent mouseEvent) throws IOException {
         User user = new LoginController().getUserDetails(userEmail.getText());
 
         if(userEmail.getText().isEmpty() && userPassword.getText().isEmpty()){
@@ -28,6 +33,12 @@ public class LoginFormController {
         }else if(user == null){
             new Alert(Alert.AlertType.ERROR,"User Not Found.!!").show();
         }else if(user.getPassword().equals(userPassword.getText())) {
+            Stage stage = new Stage();
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Homepage.fxml"))));
+            stage.show();
+            //close the login window
+            Stage currentStage = (Stage) userEmail.getScene().getWindow();
+            currentStage.close();
             new Alert(Alert.AlertType.INFORMATION,"Login Success.!!").show();
         }else{
             new Alert(Alert.AlertType.ERROR,"Invalid Password.!!").show();
