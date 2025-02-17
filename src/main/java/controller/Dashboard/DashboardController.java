8a954663ef47a56f2ab0efdb02f31e92b6938a43
chathuraruwantha-2002 <143559027+chathuraruwantha-2002@users.noreply.github.com>
2025-02-18 04:41:39 +0530

    commit charts loading
    dashboard ,sales and top sold products are done

--- src/main/java/controller/Dashboard/DashboardController.java
+++ src/main/java/controller/Dashboard/DashboardController.java
@@ -1,4 +1,24 @@
 package controller.Dashboard;
 
+import DBConnection.DBConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DashboardController {
+
+    public void dashbordChartDataLoad(){
+        //get top 6 products sold today
+        String query = "";
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            connection.prepareStatement(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void dashbordLineChartDataLoad(){
+
+    }
 }
--- src/main/java/controller/Dashboard/DashboardFormController.java
+++ src/main/java/controller/Dashboard/DashboardFormController.java
@@ -4,6 +4,9 @@ import controller.Inventory.InventoryController;
 import controller.OrderDetails.OrderDetailsController;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 
 import java.net.URL;
@@ -23,14 +26,48 @@ public class DashboardFormController implements Initializable {
     @FXML
     private Label salesLabel;
 
+    @FXML
+    private BarChart<?, ?> topSellingBarChart;
+
+    @FXML
+    private LineChart<?, ?> salesLineChart;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         showData();
+        initializeBarChart();
+        initializeLineChart();
     }
     private void showData() {
         returnsLabel.setText(String.valueOf(new OrderDetailsController().getTotalReturns()));
         alertsLabel.setText(String.valueOf(new InventoryController().getLowProductQty()));
 
+    }
+
+    private void initializeBarChart() {
+        XYChart.Series series = new XYChart.Series();
+        series.getData().add(new XYChart.Data("T-Shirts", 120));
+        series.getData().add(new XYChart.Data("Shoes", 100));
+        series.getData().add(new XYChart.Data("Accessories", 90));
+        series.getData().add(new XYChart.Data("Caps", 100));
+        series.getData().add(new XYChart.Data("Belts", 80));
+        series.getData().add(new XYChart.Data("Pants", 80));
+
+
+        topSellingBarChart.getData().add(series);
+    }
+
+    private void initializeLineChart() {
+        XYChart.Series series = new XYChart.Series();
+        series.getData().add(new XYChart.Data("Jan", 100));
+        series.getData().add(new XYChart.Data("Feb", 120));
+        series.getData().add(new XYChart.Data("Mar", 90));
+        series.getData().add(new XYChart.Data("Apr", 80));
+        series.getData().add(new XYChart.Data("May", 70));
+        series.getData().add(new XYChart.Data("Jun", 60));
+
+
+        salesLineChart.getData().add(series);
     }
 }
