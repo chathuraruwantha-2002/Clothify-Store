e741de45b6f23d5b0f1e506f383b3438d2d29dfe
chathuraruwantha-2002 <143559027+chathuraruwantha-2002@users.noreply.github.com>
2025-02-20 23:59:36 +0530

    commit reports

--- src/main/java/controller/Admin/Reports/ReportsFormController.java
+++ src/main/java/controller/Admin/Reports/ReportsFormController.java
@@ -9,23 +9,5 @@ import java.io.IOException;
 import java.util.List;
 
 public class ReportsFormController {
-    public VBox CardArea;
 
-
-    public void loadProducts(List<Product> products) {
-        // Loop through each product and create a card for it
-
-        for (Product product : products) {
-            try {
-                // Load the FXML file for a product card
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/ProductCard.fxml"));
-                Parent productCard = loader.load();
-
-                CardArea.getChildren().add(productCard);
-
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
 }
